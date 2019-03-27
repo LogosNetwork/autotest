be6d290ee4c561ea5e919d0e546235b41c55295e
ShangyanLi <smellishangyan@gmail.com>
2019-03-27 12:40:01 -0400

    minor fix

--- autotest/test_cases/test_account_creation.py
+++ autotest/test_cases/test_account_creation.py
@@ -197,7 +197,7 @@ class TestCaseMixin:
                 return True
             sleep(1)
             retries += 1
-            if retries > max_retries or time() - t0 > int(hashes / 600):
+            if retries > max_retries or time() - t0 > int(len(hashes) / 600 + 10):
                 print(self.delegates[0].blocks(hashes))
                 return False
 
